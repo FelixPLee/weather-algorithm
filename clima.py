88c87ecf81f95985f180fb0d580dae8b911b89a9
FelixPLee <felixpinheirolee@gmail.com>
2025-03-07 12:49:47 -0300

    esboço de funcionalidades

--- clima.py
+++ clima.py
@@ -1,8 +1,21 @@
 # recebe valor referente a mês e 
 while True:
+    #define variáveis no escopo global com valores que retornam erros caso não sejam fornecido valores
+    mes = 0
+    temp = 500
+
+    #Array de strings de meses do ano por extenso 
+    mesesPorExtenso = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+
     mes = int(input("Digite o número do mês desejado: "))
     #check para ver se o numero de mês é valido
     if (mes < 1) or (mes > 12):
         print('invalid number')
-    else: 
-        break
+        
+    # Recebe o valor de temperatura
+    temp = int(input("Digite o valor da temperatura máxima em Celsius refente ao mês"))
+    if (temp > 50) or (temp < -60):
+        print('invalid number')
+    else:
+        print('A temperatura máxima no mes de {mesesPorExtenso[mes]} foi de {temp}C' )
+        
