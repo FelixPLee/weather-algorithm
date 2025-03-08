895c869ef63bf2ef83b6ad717470e3b80b63955a
FelixPLee <felixpinheirolee@gmail.com>
2025-03-08 09:42:29 -0300

    work in progress

--- clima.py
+++ clima.py
@@ -1,21 +1,45 @@
 # recebe valor referente a mês e 
 while True:
     #define variáveis no escopo global com valores que retornam erros caso não sejam fornecido valores
-    mes = 0
-    temp = 500
+    mes = ''
+    temp = ''
+    mesVerificado = False
+    tempVerificado = False
 
     #Array de strings de meses do ano por extenso 
     mesesPorExtenso = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    tempMeses = [None] * 12
 
-    mes = int(input("Digite o número do mês desejado: "))
-    #check para ver se o numero de mês é valido
-    if (mes < 1) or (mes > 12):
-        print('invalid number')
-        
+    #verifica se o numero de mês é valido
+    while not(mesVerificado):
+        mes = input("Digite o número do mês desejado: ")
+        try:
+            mes = int(mes)
+            if (mes < 12) and (mes > 1):
+                mesVerificado = True
+            else:
+                print('Valor invalido tente novamente')
+                print('')
+        except ValueError:
+            print('Valor invalido tente novamente')
+            print('')
+
+
     # Recebe o valor de temperatura
-    temp = int(input("Digite o valor da temperatura máxima em Celsius refente ao mês"))
-    if (temp > 50) or (temp < -60):
-        print('invalid number')
-    else:
-        print('A temperatura máxima no mes de {mesesPorExtenso[mes]} foi de {temp}C' )
+    while not(tempVerificado):
+        temp = input("Digite o valor da temperatura máxima do mês: ")
+        #verifica se o valor de temperatura é um numero
+        try:
+            temp = float(temp)
+            if (temp > -60) and (temp < 50):
+                print('A temperatura máxima no mes de {} foi de {}°C'.format(mesesPorExtenso[mes], temp))
+                
+                tempVerificado = True
+            else: 
+                print('Valor invalido tente novamente')
+                print('')
+        except ValueError:
+            print('Valor invalido tente novamente')
+            print('')
+
         
