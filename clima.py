d8d2452bfeafa7f91d062b566a97c73e3cef08e8
FelixPLee <felixpinheirolee@gmail.com>
2025-03-10 17:15:16 -0300

    Valores preset e interface inicial

--- clima.py
+++ clima.py
@@ -1,13 +1,18 @@
 
 #array que guarda valores de temperaturas máximas
-#tempMeses = [34.3, 36, 31, 31.7,] * 12
-tempMeses =[None] * 12
+#tempMeses =[None] * 12
+tempMeses = [34.3, 36, 31, 31.7, 31, 20, 17, 42.5, 37,  32.1, 33, 23]
 while True:
-    #define variáveis no escopo global com valores que retornam erros caso não sejam fornecido valores
+    #define variáveis no escopo "global" (dentro da execução do laço) com valores que retornam erros caso não sejam fornecido valores
     mes = ''
     temp = ''
+    # definindo flags de execução de alterações
     mesVerificado = False
     tempVerificado = False
+    #array criado filtrando números 'float' sem valor decimal e os convertendo em inteiros por propósitos estéticos
+    tempMesesVisual = [int(x) if x == int(x) else x for x in tempMeses]
+
+
 
     #Array de strings de meses do ano por extenso 
     mesesPorExtenso = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
@@ -37,6 +42,7 @@ while True:
                 print('A temperatura máxima no mes de {} foi de {}°C'.format(mesesPorExtenso[mes - 1], temp))
                 print('\n')
                 tempVerificado = True
+                #guarda o valor recebido no array 'tempMeses'
                 tempMeses[mes -1] = temp
             else: 
                 print('Valor invalido tente novamente\n')
@@ -50,8 +56,9 @@ while True:
     print(linha_separadora)
     print(cabecalho) 
     print(linha_separadora)
+    #loop de todos os valores de temperatura máxima com seu respectivo mes atrelado
     for index, mes in enumerate(mesesPorExtenso) :
-        linha = f'| {mes:<10} | {str(tempMeses[index]):^18} |'
+        linha = f'| {mes:<10} | {str(tempMesesVisual[index]):^18} |'
         print(linha)
     print(linha_separadora)
 
